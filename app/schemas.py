21d8b2c0b35b4b796adefb0c751a99a84c9551c2
Abderrahim Barhoumi <120909154+AbderrahimBarhoumi@users.noreply.github.com>
2025-11-12 07:59:51 +0100

    Update schemas.py
    Added fields for first name, last name, and age to UserCreate model.

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,14 @@
-# app/schemas.py
 from pydantic import BaseModel, EmailStr
 
+#Set up models for User Register and Login
 class UserCreate(BaseModel):
+    FisrtName:str
+    LastName:str
+    Age:int
     email: EmailStr
     password: str
-
+    role: Annotated[str|None]=None
+    
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
