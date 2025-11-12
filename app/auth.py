372fb39054421d6cbef84240f6b390d05e0c6858
Abderrahim Barhoumi <120909154+AbderrahimBarhoumi@users.noreply.github.com>
2025-11-12 07:48:02 +0100

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -3,17 +3,21 @@ from app.schemas import UserCreate, UserLogin
 from app.models import User
 from app.utils import hash_password, verify_password, create_token
 
+#Initialize the router 
 router = APIRouter()
 
+# Create POST router for register 
 @router.post("/register")
 def register(user: UserCreate):
-    # Simulate user creation
+    # User create Simulation
     hashed_pw = HashPassword(user.password)
     return {"email": user.email, "hashed_password": hashed_pw}
 
+
+#POST route for Login
 @router.post("/login")
 def login(user: UserLogin):
-    # Simulate login
+    # Login Simulation
     if not verify_password(user.password, "stored_hashed_pw"):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     token = create_token(user.email)
